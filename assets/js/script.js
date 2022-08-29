85e6f91564b4a924a23ad899c14106e2cb90f923
Laura Sandoval <lausandoval1409@gmail.com>
2022-08-29 08:20:34 -0400

    last commit fuctions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-ele = document.getElementById("ele1")
+function pintar (elemento, color = 'green'){
+    elemento.style.backgroundColor = color;
+}
+let ele = document.getElementById("ele1")
 ele.style.backgroundColor = 'green'
 ele.addEventListener("click", function(){
 ele.style.backgroundColor = 'yellow'
